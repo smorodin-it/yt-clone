e72c1999dcf77d00b099325e51f9ee3012a0a4ff
Ilia <smorodin.it@gmail.com>
2022-04-19 00:05:43 +0300

    user api

--- libs/yt-backend/utils/src/lib/database.ts
+++ libs/yt-backend/utils/src/lib/database.ts
@@ -1,4 +1,4 @@
 // TODO: Put address to env variable
 
 export const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || 'mongodb://10.0.0.1:27017/youtube-clone';
+  process.env.DB_CONNECTION_STRING || 'mongodb://10.0.0.1:27017/yt-clone';
