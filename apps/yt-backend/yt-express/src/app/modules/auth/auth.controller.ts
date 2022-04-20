a62aecdaf4c77b431249611d2218401d3fc8f2b3
Ilia Smorodin <smorodin.it@gmail.com>
2022-04-20 09:12:43 +0300

    fix

--- apps/yt-backend/yt-express/src/app/modules/auth/auth.controller.ts
+++ apps/yt-backend/yt-express/src/app/modules/auth/auth.controller.ts
@@ -3,6 +3,6 @@ import { Request, Response } from 'express';
 export const loginHandler = async (
   req: Request<unknown, unknown>,
   res: Response
-): void => {
+) => {
   const { email, password } = req.body;
 };
